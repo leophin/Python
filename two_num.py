a1e115d30efb2eeee6f43cb44adb910c4b5b1114
Kishan User <kishan@gmail.com>
2018-11-22 22:11:06 +0530

    testing

--- two_num.py
+++ two_num.py
@@ -1,6 +1,6 @@
 """Author Anurag Kumar (mailto:anuragkumarak95@gmail.com)
 
-Given an array of integers, return indices of the two numbers
+ Given an array of integers, return indices of the two numbers
 such that they add up to a specific target.
 You may assume that each input would have exactly one solution,
 and you may not use the same element twice.
